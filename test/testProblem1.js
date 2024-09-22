c3e501caaf0d9c68dcf490f93ff6b664ce7ee7fc
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-22 21:04:19 +0530

    modify code using arrow functions

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -2,15 +2,19 @@ const {createDirectory , createFiles , deleteFiles} = require('../problem1.js');
 
 let numberOfFiles = 3;
 
-// createDirectory('callbackFolder' , numberOfFiles, function(dirPath , numberOfFiles){
-//     createFiles(dirPath, numberOfFiles , function(dirPath){
+
+
+// createDirectory('callbackFolder', numberOfFiles, function(dirPath, numberOfFiles ) {
+//     createFiles(dirPath, numberOfFiles , function(dirPath) {
+//         console.log("All files created. Now deleting...");
 //         deleteFiles(dirPath);
-//     })
+//     });
 // });
 
-createDirectory('callbackFolder', numberOfFiles, function(dirPath, numFiles) {
-    createFiles(dirPath, numFiles, function(dirPath) {
+
+createDirectory('callbackFolder' , numberOfFiles, (dirPath , numberOfFiles) => {
+    createFiles(dirPath , numberOfFiles , (dirPath) => {
         console.log("All files created. Now deleting...");
         deleteFiles(dirPath);
-    });
-});
+    })
+})
