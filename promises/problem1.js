f38b4009f4b46d33b9764b9fda6c9bb75b493372
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-26 12:30:31 +0530

    created new folder promises

---
+++ promises/problem1.js
@@ -0,0 +1,14 @@
+// do the following:
+// 1. Create a directory of random JSON files
+// 2. Delete those files simultaneously 
+
+const fs = require('fs/promises');
+const path = require('path');
+
+function createDirectory(dirPath){
+    fs.mkdir(dirPath).then(() => {
+        console.log("directory created successfully");
+    }).catch((error) => {
+        console.error("error creating directory -> ", error);
+    })
+}
