92ec5383613661396ca06947962d86294577d6c7
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-26 14:28:25 +0530

    add function to create number of files given by user

--- promises/problem1.js
+++ promises/problem1.js
@@ -2,11 +2,12 @@
 // 1. Create a directory of random JSON files
 // 2. Delete those files simultaneously 
 
+const { promises } = require('dns');
 const fs = require('fs/promises');
 const path = require('path');
 
 function createDirectory(dirPath) {
-    fs.mkdir(dirPath)
+    return fs.mkdir(dirPath, { recursive: true })
         .then(() => {
             console.log("directory created successfully");
         })
@@ -15,3 +16,34 @@ function createDirectory(dirPath) {
         });
 }
 
+function createFiles(dirPath, numberOfFiles) {
+    let filePromises = [];
+    for (let index = 1; index <= numberOfFiles; index++) {
+        const fileName = `file${index}.json`;
+        const filePath = path.join(dirPath, fileName);
+        const filePromise = fs.writeFile(filePath, 'file1 data')
+            .then(() => {
+                console.log(`${fileName} created`)
+            })
+            .catch((error) => {
+                console.error(`error creating ${fileName} `, error);
+            })
+        filePromises.push(filePromise);
+
+    }
+
+    return Promise.all(filePromises)
+        .then(() => {
+            console.log(`${numberOfFiles} are created successfully`);
+        })
+        .catch((error) => {
+            console.error("error is process ", error);
+        })
+}
+
+
+
+createDirectory('./promiseDir')
+    .then(() => {
+        createFiles('./promiseDir', 3);
+    })
