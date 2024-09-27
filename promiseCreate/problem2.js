fcef8acdc582429d9b3d3ac819fec3cec2377fa6
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-28 01:00:15 +0530

    export all functions to testProblem1.js and then call fileProcess to delete all files from filenames.txt

--- promiseCreate/problem2.js
+++ promiseCreate/problem2.js
@@ -7,14 +7,14 @@
 
 const fs = require('fs');
 const path = require('path');
-const filePath = './lipsum';
+const filePath = path.join(__dirname, 'lipsum.txt');;
 function readFile(filePath) {
-    return new Promise((resolve , reject) => {
-        fs.readFile(filePath ,'utf-8', (err, data) => {
-            if(err){
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
                 console.log("error in reading file");
                 reject(err);
-            }else{
+            } else {
                 console.log("file read successfully");
                 resolve(data);
             }
@@ -22,38 +22,100 @@ function readFile(filePath) {
     })
 }
 
-function writeFile(filePath , data){
-    return new Promise((resolve , reject) => {
-        fs.writeFile(filePath , data , (err) => {
-            if(err){
+function writeFile(filePath, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, data, (err) => {
+            if (err) {
                 reject(`Error writing to file ${filePath}: ${err}`)
-            }else{
-                resolve();
+            } else {
+                resolve(filePath);
             }
         })
     })
 
 }
 
-function appendFile(filePath , data){
-    return new Promise((resolve , reject) => {
-        fs.appendFile(filePath , data , (err) => {
-            if(err){
+function appendFile(filePath, data) {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(filePath, data, (err) => {
+            if (err) {
                 reject(`Error appending to file ${filePath}: ${err}`);
-            }else{
-                resolve();
+            } else {
+                resolve(filePath);
             }
         })
     })
 }
 
-function deleteFile(filePath){
-    return fs.unlink(filePath , (err) => {
-        if(err){
-            reject(`Error deleting file ${filePath}: ${err}`);
-        }else{
-            resolve();
-        }
+function deleteFile(filePath) {
+    return new Promise((resolve , reject) => {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                reject(`Error deleting file ${filePath}: ${err}`);
+            } else {
+                resolve(filePath);
+            }
+        })
+    })
+    
+}
+
+function fileProcess() {
+    return readFile(filePath)
+        .then((data) => {
+            console.log("Original file read successfully");
+
+            const upperCaseData = data.toUpperCase();
+            const newFilePath1 = path.join(__dirname, 'file1.txt');
+            return writeFile(newFilePath1, upperCaseData);
+        })
+        .then((newFilePath1) => {
+            console.log("Uppercase data written to file1.txt.");
+            return appendFile('filenames.txt', 'file1.txt\n').then(() => newFilePath1);
+        })
+        .then((newFilePath1) => {
+            console.log("file1.txt appended to filenames.txt.");
+            return readFile(newFilePath1).then((data) => {
+                const lowerCaseData = data.toLowerCase().split('. ').join('\n');
+                const newFilePath2 = path.join(__dirname, 'file2.txt');
+                return writeFile(newFilePath2, lowerCaseData)
+            });
+        })
+        .then((newFilePath2) => {
+            console.log("Lowercase data written to file2.txt.");
+            return appendFile('filenames.txt', 'file2.txt\n').then(() => newFilePath2);
+        })
+        .then((newFilePath2) => {
+            console.log("file2.txt appended to filenames.txt.");
+            return readFile(newFilePath2).then((data) => {
+                const sortedData = data.split('\n').sort().join('\n');
+                const newFilePath3 = path.join(__dirname, 'file3.txt');
+                return writeFile(newFilePath3, sortedData);
+            })
+        })
+        .then((newFilePath3) => {
+            console.log("Sorted data written to file3.txt.");
+            return appendFile('filenames.txt', 'file3.txt\n');
+        }).then(() => {
+            console.log("file3.txt appended to filenames.txt.")
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+}
+
+function deleteAllFiles() {
+    return readFile(path.join(__dirname , 'filenames.txt')).then((fileNames) => {
+        const files = fileNames.trim().split('\n');
+        console.log("file to be deleted :" , files)
+        const deletePromises = files.map((file) => deleteFile(file));
+
+        return Promise.all(deletePromises);
+    }).then(() => {
+        console.log("All files deleted successfully");
+    }).catch((err) => {
+        console.error(err);
     })
 }
 
+module.exports = { readFile, writeFile, appendFile, deleteFile, fileProcess, deleteAllFiles };
---
+++ promiseCreate/test/testProblem1.js
@@ -0,0 +1,10 @@
+const { readFile, writeFile, appendFile, deleteFile, fileProcess, deleteAllFiles } = require('../problem2.js');
+
+fileProcess()
+    .then(() => {
+        console.log("deleting files...");
+        return deleteAllFiles();
+    })
+    .catch((err) => {
+        console.error("Error in process:", err);
+    });
