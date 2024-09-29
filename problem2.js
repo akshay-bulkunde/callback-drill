6e3b4ccefb4784a60394b91bfb05a58ba3ea0210
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-29 20:55:06 +0530

    Use good practice to rewrite code

--- problem2.js
+++ problem2.js
@@ -8,137 +8,111 @@
 //         5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
 
 
+const { isUtf8 } = require('buffer');
 const fs = require('fs');
 const path = require('path');
-const filePath = './lipsum.txt';
-function readFiles(){
-    fs.readFile(filePath , 'utf-8' , (err , data) => {
-        if(err){
-            console.error("error : ")
-            console.error(err);
-        }else{
-            console.log("success : ");
-            console.log(data);
-            const upperCaseData = data.toUpperCase();
-            const newFileName1 = 'file1.txt';
-            const newFilePath1 =  path.join(__dirname , newFileName1);
-            fs.writeFile(newFilePath1 , upperCaseData , (err) => {
-                if(err){
-                    console.error("error : ");
-                    console.error(err);
-                }else{
-                    console.log(`upperCaseData is added in ${newFileName1}`);
-                    fs.appendFile('filenames.txt' , newFileName1 + '\n' , (err) => {
-                        if(err){
-                            console.error("error : ");
-                            console.error(err);
-                        }else{
-                            console.log(`${newFileName1} is appended in filename.txt successfully`);
-                            readAndSplit(newFilePath1);
 
-                        }
-                    });
-                }
-            });
-        }
-    });
+const filenamesPath = path.join(__dirname, 'filenames.txt');
 
-    
-
-
-}
-
-function readAndSplit(newFilePath){
-    fs.readFile(newFilePath , 'utf-8' , (err , data) => {
-        if(err){
-            console.error("error :");
-            console.error(err);
-        }else{
-            console.log("file read successfully");
-            console.log(data);
-            const lowerCaseData = data.toLowerCase().split('. ').join('\n');
-            console.log(lowerCaseData)
-            const newFileName2 = 'file2.txt';
-            const newFilePath2 = path.join(__dirname , newFileName2);
-            fs.writeFile(newFilePath2 , lowerCaseData , (err) => {
-                if(err){
-                    console.error("error : ");
-                    console.log(err);
-                }else{
-                    console.log(`lowercase data is added in ${newFileName2}`);
-                    fs.appendFile('filenames.txt' , newFileName2 + '\n', (err) => {
-                        if(err){
-                            console.error("error : ");
-                            console.log(err);
-                        }else{
-                            console.log(`${newFileName2} is added in filenames.txt`)
-                            sortData(newFilePath2)
-                        }
-                    })
-                }
-            })
-
+function readFiles(filePath, callback) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error in file reading 1: ${err}`);
+        } else {
+            console.log(`File read successfully : ${data}`);
+            callback(data);
         }
     })
 }
 
-function sortData(newFilePath2){
-    fs.readFile(newFilePath2 , 'utf-8' , (err , data) => {
-        if(err){
-            console.error("error : ");
-            console.log(err);
-        }else{
-            console.log("file read successfully : ");
-            console.log(data);
-            const sortedData = data.split('. ').sort();
-            const newFileName3 = 'file3.txt';
-            const newFilePath3 = path.join(__dirname , newFileName3);
-            fs.writeFile(newFilePath3  , sortedData.join('. ') , (err) => {
-                if(err){
-                    console.error("error : ");
-                    console.error(err);
-                }else{
-                    console.log(`sortedData added in ${newFileName3}`);
-                    console.log(sortData);
-                    fs.appendFile('filenames.txt' , newFileName3 + '\n' , (err) => {
-                        if(err){
-                            console.error("error : ");
-                            console.log(err);
-                        }else{
-                            console.log(`${newFileName3} is added in filenames.txt successfully`);
-                            deleteFiles();
-                        }
-                    })
-                }
+function updateFilenames(fileName, callback) {
+
+
+    fs.appendFile(filenamesPath, `${fileName}\n`, (err) => {
+        if (err) {
+            console.error(`Erron in appending ${fileName} in filenames.txt`);
+        } else {
+            console.log(`${fileName} is appended successfully`);
+            callback();
+        }
+    })
+}
+
+function writeUpperCaseFile(data, callback) {
+    const upperCaseData = data.toUpperCase();
+    const newFileName1 = 'file1.txt';
+    const newFilePath1 = path.join(__dirname, newFileName1);
+    fs.writeFile(newFilePath1, upperCaseData, (err) => {
+        if (err) {
+            console.error(`Error in writing data ti ${newFileName1} : ${err}`);
+        } else {
+            console.log(`UpperCaseData added in ${newFileName1} successfully`);
+            updateFilenames(newFileName1, () => {
+                callback(newFilePath1);
             })
         }
     })
 }
 
-function deleteFiles(){
-    fs.readFile('filenames.txt' , 'utf-8' , (err , filenames) => {
-        if(err){
-            console.error("error : ");
-            console.error(err);
-        }else{
-            console.log("file read successfully");
-            const files = filenames.trim().split('\n');
-            files.forEach((file) => {
-                fs.unlink(file , (err) => {
-                    if(err){
-                        console.error("error :");
-                        console.error(err);
-                    }else{
-                        console.log(`${file} successfully deleted`)
-                    }
+function writeLowerCaseFile(newFilePath1, callback) {
+    readFiles(newFilePath1, (data) => {
+        const lowerCaseData = data.toLowerCase().split('.').join('.\n');
+        let newFileName2 = 'file2.txt';
+        const newFilepath2 = path.join(__dirname, newFileName2);
+        fs.writeFile(newFilepath2, lowerCaseData, (err) => {
+            if (err) {
+                console.error(`Error in writing lowerCaseData in ${newFileName2}`);
+            } else {
+                console.log(`lowerCaseData added in ${newFileName2} successfully`);
+                updateFilenames(newFileName2, () => {
+                    callback(newFilepath2);
                 })
-            })
-        }
-
-    }) 
+            }
+        })
+    })
 }
 
-module.exports = {readFiles};
+function sortData(filePath, callback) {
+    readFiles(filePath, (data) => {
 
+        console.log(`File read successfully`);
+        const sortedData = data.split('.').map(s => s.trim()).sort().join('. ');
+        const newFileName3 = 'file3.txt';
+        const newFilepath3 = path.join(__dirname, newFileName3);
+        fs.writeFile(newFilepath3, sortedData, (err) => {
+            if (err) {
+                console.error(`Error in adding sortedData in ${newFileName3} : ${err}`);
+            } else {
+                console.log(`${sortedData} is added in ${newFileName3} successfully`);
+                updateFilenames(newFileName3, () => {
+                    callback(newFilepath3);
+                })
+            }
+        })
 
+    })
+}
 
+function deleteFiles(callback) {
+    readFiles(filenamesPath, (data) => {
+        const filesToBeDeleted = data.split('\n').filter(Boolean);
+        console.log(`Files to be Deleted : ${filesToBeDeleted}`);
+        let deletedFiles = 0;
+        filesToBeDeleted.forEach((file) => {
+            let filePath = path.join(__dirname, file);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(`Erron in deleting ${file}`);
+                } else {
+                    console.log(`${file} deleted successfully`);
+                    deletedFiles++;
+                    if (deletedFiles === filesToBeDeleted.length) {
+                        callback();
+                    }
+                }
+            })
+        })
+    })
+}
+
+module.exports = { readFiles, updateFilenames, writeUpperCaseFile, writeLowerCaseFile, sortData, deleteFiles }
