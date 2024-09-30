9b5938c2d088620d97fabc5f7e92968de3987318
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-30 11:27:17 +0530

    Add function to resolve a promise after 3 sec

---
+++ Practice-Drill/problem1.js
@@ -0,0 +1,25 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+    5. Print out "Program complete" when the promise completes after 3 seconds
+
+    HINT: Use setTimeout for the delay
+*/
+
+function runProgram() {
+    console.log("Program started");
+    let newPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Program complete");
+        }, 3000);
+    })
+    console.log(newPromise);
+    console.log("Program in progress...");
+    newPromise.then((message) => {
+        console.log(message);
+    })
+}
+
+runProgram();
