5e2e4b4d581587acdac76b824ba137b4f75c356e
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-30 11:38:46 +0530

    Remove unwanted comments

--- Practice-Drill/problem2.js
+++ Practice-Drill/problem2.js
@@ -1,16 +1,3 @@
-/*
-    1. Print out "Program started" at the start of your code
-    2. Create a Promise that resolves after 3 seconds
-       and rejects after 2 seconds
-    3. Log out the promise while it's pending
-    4. Print out "Program in progress..." as well
-
-    5. Print out "Program complete" if the promise fulfills
-    6. Print out "Program failure" if the promise rejects
-
-    HINT: Use setTimeout for the delay
-*/
-
 function runProgram() {
     console.log("Program started");
     let newPromise = new Promise((resolve, reject) => {
